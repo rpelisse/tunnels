2e663ffd08e9ed0c6cf8e18124464c11b109f85e
Romain PELISSE <belaran@gmail.com>
2014-05-22 17:38:35 +0200

    Add ports forwarding for JBoss AS console

--- tunnel-forward.sh
+++ tunnel-forward.sh
@@ -17,10 +17,13 @@ firewallOp stop
 openTcpPort ${REMOTE_SSH_PORT}
 openTcpPort ${REMOTE_DV_SQL_PORT}
 openTcpPort ${REMOTE_CLI_PORT}
+openTcpPort ${REMOTE_HTTP_PORT}
+openTcpPort ${REMOTE_HTTP_CONSOLE_PORT}
 openTcpPort ${REMOTE_MSSQL_PORT}
 openTcpPort ${REMOTE_MSSQL_DEV_PORT}
 openTcpPort ${REMOTE_SAS_PORT}
 openTcpPort ${REMOTE_LDAP_PORT}
 openTcpPort ${REMOTE_DB2_PORT}
 openTcpPort ${REMOTE_ORACLE_PORT}
+openTcpPort ${REMOTE_ORACLE_PROD_PORT}
 firewallOp start
--- tunnel.sh
+++ tunnel.sh
@@ -12,9 +12,11 @@ ${dry_run_mode} ssh -f -N -v -R ${REMOTE_SSH_PORT}:localhost:${LOCAL_SSH_PORT} \
                 -R ${REMOTE_CLI_PORT}:localhost:${LOCAL_CLI_PORT} \
                 -R ${REMOTE_DV_SQL_PORT}:localhost:${LOCAL_DV_SQL_PORT} \
                 -R ${REMOTE_HTTP_PORT}:localhost:${LOCAL_HTTP_PORT} \
+                -R ${REMOTE_HTTP_CONSOLE_PORT}:localhost:${LOCAL_HTTP_CONSOLE_PORT} \
                 -R ${REMOTE_MSSQL_PORT}:${MSSQL_SERVER}:${MSSQL_PORT} \
                 -R ${REMOTE_MSSQL_DEV_PORT}:${MSSQL_SERVER_DEV}:${MSSQL_DEV_PORT} \
                 -R ${REMOTE_ORACLE_PORT}:${ORACLE_SERVER}:${ORACLE_PORT} \
+                -R ${REMOTE_ORACLE_PROD_PORT}:${ORACLE_PROD_SERVER}:${ORACLE_PROD_PORT} \
                 -R ${REMOTE_DB2_PORT}:${DB2_SERVER}:${DB2_PORT} \
                 -R ${REMOTE_SAS_PORT}:${SAS_SERVER}:${SAS_PORT} \
                 -R ${REMOTE_LDAP_PORT}:${LDAP_SERVER}:${LDAP_PORT} \
