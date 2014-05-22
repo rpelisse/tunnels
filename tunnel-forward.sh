f6025a3c5a8dd8f9ccf4287c16b1bcc243424e42
Romain PELISSE <belaran@gmail.com>
2014-05-22 09:37:25 +0200

    Code cleaning and adding new forwards

--- tunnel-forward.sh
+++ tunnel-forward.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 source ports.sh
 
-sudo /bin/systemctl stop  firewalld.service
-sudo lokkit -q -p ${REMOTE_SSH_PORT}:tcp
-sudo lokkit -q -p ${REMOTE_DV_SQL_PORT}:tcp
-sudo lokkit -q -p ${REMOTE_CLI_PORT}:tcp
-sudo lokkit -q -p ${REMOTE_MSSQL_PORT}:tcp
-sudo /bin/systemctl start  firewalld.service
+openTcpPort() {
+  local port=${1}
+
+  sudo lokkit --nostart -q -p ${port}:tcp
+}
+
+firewallOp() {
+  local op=${1}
+
+  sudo /bin/systemctl "${op}" firewalld.service
+}
+
+firewallOp stop
+openTcpPort ${REMOTE_SSH_PORT}
+openTcpPort ${REMOTE_DV_SQL_PORT}
+openTcpPort ${REMOTE_CLI_PORT}
+openTcpPort ${REMOTE_MSSQL_PORT}
+openTcpPort ${REMOTE_MSSQL_DEV_PORT}
+openTcpPort ${REMOTE_SAS_PORT}
+openTcpPort ${REMOTE_LDAP_PORT}
+openTcpPort ${REMOTE_DB2_PORT}
+openTcpPort ${REMOTE_ORACLE_PORT}
+firewallOp start
