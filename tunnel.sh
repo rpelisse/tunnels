98d15758b474b8d0b1b45a8bfbd80f322d48b882
Romain PELISSE <belaran@gmail.com>
2014-05-22 09:37:13 +0200

    Add other DS (DB2, Oracle, M dev)

--- tunnel.sh
+++ tunnel.sh
@@ -3,10 +3,15 @@
 source ports.sh
 
 echo "Starting tunnel to remote ${REMOTE_SERVER}..."
-ssh -f -N -v -R ${REMOTE_SSH_PORT}:localhost:${LOCAL_SSH_PORT} \
+echo ssh -f -N -v -R ${REMOTE_SSH_PORT}:localhost:${LOCAL_SSH_PORT} \
                 -R ${REMOTE_CLI_PORT}:localhost:${LOCAL_CLI_PORT} \
                 -R ${REMOTE_DV_SQL_PORT}:localhost:${LOCAL_DV_SQL_PORT} \
-		-R ${REMOTE_HTTP_PORT}:localhost:${LOCAL_HTTP_PORT} \
-		-R ${REMOTE_MSSQL_PORT}:${MSSQL_SERVER}:${MSSQL_PORT} \
+                -R ${REMOTE_HTTP_PORT}:localhost:${LOCAL_HTTP_PORT} \
+                -R ${REMOTE_MSSQL_PORT}:${MSSQL_SERVER}:${MSSQL_PORT} \
+                -R ${REMOTE_MSSQL_DEV_PORT}:${MSSQL_SERVER_DEV}:${MSSQL_DEV_PORT} \
+                -R ${REMOTE_ORACLE_PORT}:${ORACLE_SERVER}:${ORACLE_PORT} \
+                -R ${REMOTE_DB2_PORT}:${DB2_SERVER}:${DB2_PORT} \
+                -R ${REMOTE_SAS_PORT}:${SAS_SERVER}:${SAS_PORT} \
+                -R ${REMOTE_LDAP_PORT}:${LDAP_SERVER}:${LDAP_PORT} \
                 "${REMOTE_USER}@${REMOTE_SERVER}"
 echo 'Tunnel closed.'
